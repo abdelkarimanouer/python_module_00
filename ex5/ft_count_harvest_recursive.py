643db31a04592e09fe43137ebf0c8e77cc61946b
abdelkarimanouer <abdelkarimanouer1337@gmail.com>
2025-12-11 12:05:35 +0100

    learn recursive in python

--- ex5/ft_count_harvest_recursive.py
+++ ex5/ft_count_harvest_recursive.py
@@ -1,13 +1,10 @@
 def ft_count_harvest_recursive():
-    def recurse():
-        pass
     days = int(input("Days until harvest: "))
     i = 1
-    if i <= days:
-        print("Day", i)
-        i += 1
-        ft_count_harvest_recursive()
-    else :
-        return ;
 
-ft_count_harvest_recursive()
+    def recurse(days, i):
+        if i <= days:
+            print("Day", i)
+            i += 1
+            recurse(days, i)
+    recurse(days, i)
