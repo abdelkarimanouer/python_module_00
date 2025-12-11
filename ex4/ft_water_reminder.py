349cbe14add9487cfa26704190b33cf681f1f2a0
abdelkarimanouer <abdelkarimanouer1337@gmail.com>
2025-12-11 10:53:11 +0100

    learn how to use condtions in python

---
+++ ex4/ft_water_reminder.py
@@ -0,0 +1,9 @@
+def ft_water_reminder():
+    days = int(input("Enter plant age in days: "))
+    if days > 2:
+        print("Water the plants!")
+    else:
+        print("Plants are fine")
+
+
+ft_water_reminder()
